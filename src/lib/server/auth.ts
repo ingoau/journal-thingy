3482cc65fe2027e311917396f1a8d5c5d468f0bb
Ingo Wolf <me@ingo.au>
2026-07-10 22:28:17 +1000

    add email otp
    Co-authored-by: Williamnumismatics <williamnumismatics@gmail.com>
    Co-authored-by: Lordseriouspig <lordseriouspig@gmail.com>

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,6 +4,7 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
+import { emailOTP } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
@@ -11,6 +12,21 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, { provider: 'sqlite' }),
 	emailAndPassword: { enabled: true },
 	plugins: [
+		emailOTP({
+			async sendVerificationOTP({ email, otp, type }) {
+				switch (type) {
+					case 'sign-in':
+						console.log(`Sign-in OTP: ${otp} for ${email}`);
+						break;
+					case 'email-verification':
+						console.log(`Email verification OTP: ${otp} for ${email}`);
+						break;
+					case 'forget-password':
+						console.log(`Password reset OTP: ${otp} for ${email}`);
+						break;
+				}
+			}
+		}),
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
 	]
 });
