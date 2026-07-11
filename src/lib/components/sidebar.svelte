9eae180f47741bfe6bb9c9cf6af829a61773cec4
Ingo Wolf <me@ingo.au>
2026-07-11 23:28:21 +1000

    Add cool sidebar animation

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import type { User } from 'better-auth';
-	import { IconBook2, IconChevronUp, IconLogout } from '@tabler/icons-svelte';
+	import { IconBook2, IconChevronRight, IconChevronUp, IconLogout } from '@tabler/icons-svelte';
 	import { cn } from '$lib/utils';
 	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
 	import { authClient } from '$lib/auth-client';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import Button from './ui/button/button.svelte';
+
+	let isOpen = $state(true);
 
 	let { user }: { user: User } = $props();
 
@@ -32,8 +35,35 @@
 	</a>
 {/snippet}
 
-<div class="top-0 left-0 p-8 fixed h-full flex flex-col gap-4 w-sm overflow-hidden">
-	<IconBook2 size={32} />
+<Button
+	variant="ghost"
+	size="icon"
+	class={cn(
+		'size-12 fixed group top-8 left-8 z-20 bg-background',
+		!isOpen && 'top-4 left-4 scale-75'
+	)}
+	onclick={() => (isOpen = !isOpen)}
+>
+	<IconBook2
+		size={32}
+		class="absolute size-10 group-hover:opacity-25 group-hover:scale-90 duration-200"
+	/>
+	<IconChevronRight
+		size={32}
+		class={cn(
+			'absolute size-8 opacity-0 group-hover:opacity-100 group-hover:scale-95 duration-200 scale-x-100',
+			isOpen && '-scale-x-100'
+		)}
+	/>
+</Button>
+
+<div
+	class={cn(
+		'top-0 left-0 p-8 fixed h-full flex flex-col gap-4 w-sm overflow-hidden duration-200 bg-background z-10',
+		!isOpen && '-translate-x-full'
+	)}
+>
+	<div class="h-12"></div>
 	{#each items as item, index (index)}
 		{@render navLink(item.href, item.name)}
 	{/each}
