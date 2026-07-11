8d19441d3875bdc50a1f6cc6f5bffe4224a010ab
Williamnumismatics <williamnumismatics@gmail.com>
2026-07-11 10:29:29 +1000

    add dialog

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { IconNotes, IconChevronUp } from '@tabler/icons-svelte';
+
+	const items = [
+		{ name: 'Timeline', href: '/' },
+		{ name: 'Calender', href: '/' }
+	];
+    </script>
+
+<div class="top-0 left-0 p-6 fixed h-full flex flex-col gap-2">
+	<IconNotes stroke={1} class="" />
+	{#each items as item, index (index)}
+		<a href={item.href} class="font-heading text-2xl hover:text-foreground text-muted-foreground">
+			{item.name}
+		</a>
+	{/each}
+	<div class="grow"></div>
+	<a href="/" class="font-heading text-2xl hover:text-foreground text-muted-foreground">
+		Settings
+	</a>
+	<div class="font-heading text-2xl text-foreground flex flex-row items-center gap-2">
+		willgob <IconChevronUp stroke={2} />
+	</div>
+</div>
