9e5744f579b06c27ecc1a931e12d368318c25857
Ingo Wolf <me@ingo.au>
2026-07-12 11:55:39 +1000

    idk do some stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,14 +5,12 @@ if (!url) throw new Error('TURSO_DATABASE_URL or DATABASE_URL must be set');
 
 const authToken =
 	process.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_DATABASE_TURSO_AUTH_TOKEN;
+const isTurso = url.startsWith('libsql:');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: {
-		url,
-		authToken
-	},
+	dialect: isTurso ? 'turso' : 'sqlite',
+	dbCredentials: isTurso ? { url, authToken } : { url },
 	verbose: true,
 	strict: true
 });
